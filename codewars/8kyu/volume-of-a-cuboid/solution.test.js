d0b85df64be8eae79e2e5783b037835bfdb312fe
riddlew <riddle.w@gmail.com>
2023-04-16 23:52:53 -0400

    8kyu/volume-of-a-cuboid

---
+++ codewars/8kyu/volume-of-a-cuboid/solution.test.js
@@ -0,0 +1,10 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+import Kata from './solution.js';
+
+describe("Tests", () => {
+  it("Example tests", () => {
+    assert.equal(Kata.getVolumeOfCuboid(1,2,2),  4);
+    assert.equal(Kata.getVolumeOfCuboid(6,2,5), 60);
+  });
+});
