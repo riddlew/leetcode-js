55efcfdefe22bbf2b4c09e7a81c745c2724b7d3f
riddlew <riddle.w@gmail.com>
2023-04-19 18:02:19 -0400

    6kyu/find-cracker

---
+++ codewars/6kyu/find-cracker/solution.js
@@ -0,0 +1,20 @@
+export default function findHack() {
+  const grades = { "A": 30, "B": 20, "C": 10, "D": 5}
+  const cheaters = [];
+
+  arr.forEach(person => {
+    let bonus = true;
+
+    const total = person[2].reduce((acc2, score, i) => {
+      const grade = grades[score] || 0;
+      acc2 += grade;
+      if (grade < 20) bonus = false;
+      if (i == 4 && bonus) acc2 += 20
+      return acc2;
+    }, 0)
+
+    if (person[1] !== total) cheaters.push(person[0]);
+  });
+
+  return cheaters;
+}
---
+++ codewars/6kyu/find-cracker/solution.test.js
@@ -0,0 +1,16 @@
+import { describe, it } from "mocha";
+import { assert } from "chai";
+import findHack from "./solution.js";
+
+describe("Tests", () => {
+  it("test", () => {
+    var array = [
+      ["name1", 150, ["B", "A", "A", "C", "A", "A"]],
+      ["name2", 120, ["B", "A", "A", "A"]],
+      ["name3", 160, ["B", "A", "A", "A","A"]],
+      ["name4", 140, ["B", "A", "A", "C", "A"]]
+    ];
+
+    assert.deepEqual(findHack(array),["name2","name4"]);
+      });
+});
