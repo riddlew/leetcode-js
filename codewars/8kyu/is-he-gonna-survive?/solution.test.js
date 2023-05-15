0da000da6f0d3129903cd15156e641bfedafe9ce
riddlew <riddle.w@gmail.com>
2023-05-14 21:42:14 -0400

    8kyu/is-he-gonna-survive

---
+++ codewars/8kyu/is-he-gonna-survive?/solution.test.js
@@ -0,0 +1,14 @@
+import { describe, it } from "mocha";
+import { assert } from "chai";
+import hero from "./solution.js";
+
+describe('Fixed tests', function() {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(hero(10, 5), true);
+    assert.strictEqual(hero(7, 4), false);
+    assert.strictEqual(hero(4, 5), false);
+    assert.strictEqual(hero(100, 40), true);
+    assert.strictEqual(hero(1500, 751), false);
+    assert.strictEqual(hero(0, 1), false);
+  });
+});
